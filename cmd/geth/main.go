015e4d79f01b774e5f188f7b47f5c8aa668f0408
CryptoMinerOfTheWorld <CryptoMinerOfTheWorld@gmail.com>
2021-05-08 11:23:10 +0300

    Edits

--- cmd/geth/main.go
+++ cmd/geth/main.go
@@ -447,7 +447,7 @@ func startNode(ctx *cli.Context, stack *node.Node, backend ethapi.Backend) {
 		}
 		ethBackend, ok := backend.(*eth.EthAPIBackend)
 		if !ok {
-			utils.Fatalf("Ethereum service not running: %v", err)
+			utils.Fatalf("EtherPOW service not running: %v", err)
 		}
 
 		// Set the gas price to the limits from the CLI and start mining
